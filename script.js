bc80c359d0bcb483c3118e95366f0d9a3feef768
zulfikarelreal <50861245+zulfikarelreal@users.noreply.github.com>
2024-07-24 16:26:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const namaUser = document.getElementById("nama-user");
+const cekButton = document.getElementById("cek-button");
+const resultText = document.getElementById("result-text");
+
+// kumpulan data array khodam
+let dataKhodam = [
+  "kodok zuma",
+  "hellhound",
+  "nemesis berak",
+  "windah botak",
+  "kunti bogel",
+  "roro jonggrang",
+];
+
+// tombol cek akan bereaksi setelah di klik dengan function(e)
+cekButton.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let nama = namaUser.value;
+
+  if (nama === "") {
+    resultText.textContent = "Silakan masukkan nama Anda!";
+  } else {
+    let randomIndex = Math.floor(Math.random() * dataKhodam.length);
+    let randomKhodam = dataKhodam[randomIndex];
+
+    resultText.textContent = `Khodam ${nama} adalah "${randomKhodam}"`; // output
+    namaUser.value = ""; // kolom input akan kosong setelah klik tombol cek
+  }
+});
